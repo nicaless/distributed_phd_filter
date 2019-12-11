ac122b3c0b1e92ec137b915d8d7806b5340ab254
Nicole Fronda <nfronda@gmail.com>
2019-12-10 22:05:26 -0800

    done with centralized gmphd

--- PHDFilterNode.py
+++ PHDFilterNode.py
@@ -2,6 +2,7 @@ from copy import deepcopy
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from operator import attrgetter
 import scipy.cluster.vq as vq
 from scipy.stats import norm
 from target import Target
@@ -10,36 +11,34 @@ from target import Target
 class PHDFilterNode:
     def __init__(self,
                  birthgmm,
+                 prune_thresh=1e-6,
+                 merge_thresh=0.01,
+                 max_comp=100,
                  region=[(-50, 50), (-50, 50)]
                  ):
         self.birthgmm = birthgmm
+        self.prune_thresh = prune_thresh
+        self.merge_thresh = merge_thresh
+        self.max_components = max_comp
+        self.region = region
         self.targets = []
         self.survival_prob = 0.98
         self.detection_probability = 0.95
-        self.clutter_intensity = 0.005  # clutter total is 5
-        self.region = region
+        self.clutter_intensity = 0.0005  # clutter total is 5
+        # TODO parametrize based on region and clutter intensity total (clutter lambda)
 
         # prediction results
         self.predicted_pos = []
-        self.predicted_weights = []
         self.predicted_targets = []
 
         # update results
-        self.updated_weights = []
+        self.updated_targets = []
 
-        # # resample results
-        # self.resampled_pos = []
-        # self.resampled_weights = []
-        # self.resampled_num_targets = len(self.resampled_pos)
-        #
-        # # target centroids
-        # self.centroids = []
-        # self.centroid_movement = {}
-        # self.est_num_targets = len(self.centroids)
+        # pruned results
+        self.pruned_targets = []
 
-        # rescaled weights (after fusion)
-        self.rescaled_weights = []
-        self.adjusted_centroids = []
+        # merged results
+        self.merged_targets = []
 
     def predict(self):
         # Existing Targets
@@ -53,12 +52,10 @@ class PHDFilterNode:
 
         all_targets = updated + born
         predicted_pos = []
-        predicted_weights = []
         for p in all_targets:
             predicted_pos.append(p.get_measurement())
-            predicted_weights.append(p.weight)
+
         self.predicted_pos = predicted_pos
-        self.predicted_weights = predicted_weights
         self.predicted_targets = all_targets
 
     def update(self, measurements):
@@ -83,8 +80,10 @@ class PHDFilterNode:
             weightsum = 0
             for index, comp in enumerate(self.predicted_targets):
                 obs_probability = dmvnorm(nu[index], s[index], m)
-                newcomp_weight = comp.weight * self.detection_probability * obs_probability
-                newcomp_state = comp.state + np.dot(K, m - nu[index])
+                newcomp_weight = float(comp.weight *
+                                       self.detection_probability *
+                                       obs_probability)
+                newcomp_state = comp.state + np.dot(K[index], m - nu[index])
                 newcomp_state_cov = comp.state_cov
                 newgmmpartial.append(Target(init_weight=newcomp_weight,
                                             init_state=newcomp_state,
@@ -98,108 +97,77 @@ class PHDFilterNode:
 
             newgmm.extend(newgmmpartial)
 
-        self.targets = newgmm
-        new_weights = [t.weight for t in self.targets]
-        self.updated_weights = new_weights
+        self.updated_targets = newgmm
 
-    def resample(self):
-        particle_mass = np.sum(self.updated_weights)
-        true_target_indices = Resample(np.array(self.updated_weights) / particle_mass)
-        true_particles = [self.predicted_pos[i] for i in true_target_indices]
-        true_weights = [self.updated_weights[i] * np.ceil(particle_mass)
-                        for i in true_target_indices]
+    def prune(self):
+        prunedgmm = list(filter(lambda comp: comp.weight > self.prune_thresh,
+                                self.updated_targets))
+        self.pruned_targets = prunedgmm
 
-        self.resampled_pos = true_particles
-        self.resampled_weights = true_weights
-        self.resampled_num_targets = len(self.resampled_pos)
+    def merge(self):
+        weightsums = sum([comp.weight for comp in self.pruned_targets])
+        sourcegmm = [deepcopy(comp) for comp in self.pruned_targets]
+        newgmm = []
 
-    def estimate(self, rescale=False):
-        particle_positions_matrix = np.zeros((len(self.resampled_pos), 2))
-        for p in range(len(self.resampled_pos)):
-            particle_positions_matrix[p][0] = self.resampled_pos[p][0]
-            particle_positions_matrix[p][1] = self.resampled_pos[p][1]
-        if rescale:
-            estimated_total_targets = int(np.round(np.sum(self.rescaled_weights), 0))
-        else:
-            estimated_total_targets = int(np.round(np.sum(self.resampled_weights), 0))
+        while len(sourcegmm) > 0:
 
-        # estimated_total_targets = max(estimated_total_targets,
-        #                               len(self.birth_models))
-        if estimated_total_targets == 0:
-            centroids = []
-        elif len(self.resampled_pos) == 1:
-            centroids = particle_positions_matrix
-        else:
-            centroids, idx = vq.kmeans2(particle_positions_matrix,
-                                        estimated_total_targets)
-            centroids = centroids
+            # Get Weightiest Component delete
+            w = np.argmax(comp.weight for comp in sourcegmm)
+            weightiest = sourcegmm[w]
+            del sourcegmm[w]
 
-        self.centroids = centroids
+            # Find all nearby components and delete
+            distances = [float(
+                np.dot(
+                    np.dot((comp.state - weightiest.state).T,
+                           np.linalg.inv(comp.state_cov)),
+                    comp.state - weightiest.state))
+                for comp in sourcegmm]
 
-    # psi = detection_prob * prob we receive measurement particle pos
-    def CalcPsi(self, measurements, positions, measurement_variance=20):
-        psi_mat = {}
-        for i, m in enumerate(measurements):
-            psi_mat[i] = []
-            x1 = m[0][0]
-            y1 = m[1][0]
-            for j, p in enumerate(positions):
-                x2 = p[0][0]
-                y2 = p[1][0]
-                dist = abs(math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2))
-                prob_m_is_p = norm.sf(dist, 0, measurement_variance)
-                psi = self.DetectionProb((x2, y2)) * prob_m_is_p
-                psi_mat[i].append(psi)
-        return psi_mat
+            tosubsume = np.array([d <= self.merge_thresh for d in distances])
+            subsumed = [weightiest]
+            if any(tosubsume):
+                subsumed.extend(list(np.array(sourcegmm)[tosubsume]))
+                sourcegmm = list(np.array(sourcegmm)[~tosubsume])
 
-    def DetectionProb(self, target):
-        x = target[0]
-        y = target[1]
-        if x < self.region[0][0] or x > self.region[0][1] \
-                or y < self.region[1][0] or y > self.region[1][1]:
-            return 0
-        else:
-            return self.detection_probability
+            # Create new component from subsumed components
+            newcomp_weight = sum([comp.weight for comp in subsumed])
 
-    # for each measurement calculate: Ck = sum(psi * weight of particle)
-    def CalcCk(self, psi_mat):
-        Ck_m = []
-        for i, particles_psi in psi_mat.items():
-            Ck = 0
-            for j, psi in enumerate(particles_psi):
-                Ck += psi * self.predicted_weights[j]
-            Ck_m.append(Ck)
-        return Ck_m
+            newcomp_state = np.sum(
+                np.array([
+                    comp.weight * comp.state
+                    for comp in subsumed]), 0) / newcomp_weight
+
+            newcomp_cov = np.sum(
+                np.array([
+                    comp.weight *
+                    (comp.state_cov + (weightiest.state - comp.state) *
+                     (weightiest.state - comp.state).T)
+                    for comp in subsumed]), 0) / newcomp_weight
+
+            newcomp = Target(init_weight=newcomp_weight,
+                             init_state=newcomp_state,
+                             init_cov=newcomp_cov)
+
+            newgmm.append(newcomp)
+
+        # Keep no more than max components
+        newgmm.sort(key=attrgetter('weight'), reverse=True)
+        newweightsum = sum([comp.weight for comp in newgmm])
+        self.merged_targets = newgmm[:self.max_components]
+        weightnorm = newweightsum / float(weightsums)
+        for comp in newgmm:
+            comp.weight *= weightnorm
+        self.merged_targets = newgmm
 
     def plot(self, k, folder='results'):
-        # plot all 4 steps
+        # Plot Extracted States
 
-        # plot predicted new positions of particles
-        plt.xlim([-100, 100])
-        plt.ylim([-100, 100])
+        plt.xlim(self.region[0])
+        plt.ylim(self.region[1])
 
-        x = []
-        y = []
-        for t in self.predicted_pos:
-            x.append(t[0][0])
-            y.append(t[1][0])
-        plt.scatter(x, y, label='prediction')
-
-        # plot resampled positions
-        x = []
-        y = []
-        for t in self.resampled_pos:
-            x.append(t[0][0])
-            y.append(t[1][0])
-        plt.scatter(x, y, label='resample')
-
-        # plot centroid
-        x = []
-        y = []
-        for t in self.centroids:
-            x.append(t[0])
-            y.append(t[1])
-        plt.scatter(x, y, label='centroid', color='black', s=50)
+        x, y = self.extractstates()
+        plt.scatter(x, y)
 
         plt.legend()
         plt.savefig('{folder}/{k}.png'.format(folder=folder, k=k))
@@ -207,50 +175,32 @@ class PHDFilterNode:
 
     # TODO: add a reset
     def step_through(self, measurements, folder='results'):
-        for i, m in measurements.items():
+        if not isinstance(measurements, dict):
             self.predict()
-            self.update(m)
-            self.resample()
-            self.estimate()
-            self.plot(i, folder=folder)
+            self.update(measurements)
+            self.prune()
+            self.merge()
+            self.targets = self.merged_targets
+            self.plot(0, folder=folder)
 
-            self.particles[i] = self.resampled_pos
-            self.current_particles = self.resampled_pos
-            self.num_current_particles = len(self.current_particles)
-            self.weights[i] = self.resampled_weights
-            self.current_weights = self.resampled_weights
-            self.centroid_movement[i] = self.centroids
-            self.est_num_targets = len(self.centroids)
+        else:
+            for i, m in measurements.items():
+                self.predict()
+                self.update(m)
+                self.prune()
+                self.merge()
+                self.targets = self.merged_targets
+                self.plot(i, folder=folder)
 
-    def plot_centroids(self):
-        targets = {}
-        for i, cs in self.centroid_movement.items():
-            for j, c in enumerate(cs):
-                if j not in targets.keys():
-                    targets[j] = [c]
-                else:
-                    targets[j].append(c)
-
-        for t, pos in targets.items():
-            x = []
-            y = []
-            for p in pos:
-                x.append(p[0])
-                y.append(p[1])
-            plt.scatter(x, y, label=t)
-
-        plt.legend()
-        plt.savefig('centroids.png')
-
-    def weightScale(self, avg_num_targets):
-        rescaled_weights = []
-        local_num_targets = len(self.centroids)
-        if local_num_targets == 0:
-            local_num_targets = 0.01
-        for w in self.current_weights:
-            scale = avg_num_targets / float(local_num_targets)
-            rescaled_weights.append(scale * w)
-        self.rescaled_weights = rescaled_weights
+    def extractstates(self, thresh=0.2):
+        x = []
+        y = []
+        for comp in self.targets:
+            if comp.weight > thresh:
+                for _ in range(int(np.ceil(comp.weight))):
+                    x.append(comp.state[0][0])
+                    y.append(comp.state[1][0])
+        return x, y
 
 
 def dmvnorm(state, cov, obs):
@@ -267,28 +217,6 @@ def dmvnorm(state, cov, obs):
     dist = obs - state
     part3 = np.exp(-0.5 * np.dot(np.dot(dist.T, np.linalg.inv(cov)), dist))
     return part1 * part2 * part3
-
-
-# Copied form here:
-# https://filterpy.readthedocs.io/en/latest/_modules/filterpy/monte_carlo/resampling.html#systematic_resample
-def Resample(weights):
-    N = weights.size
-
-    cum_sum_weights = np.cumsum(weights)
-
-    random_positions = (np.random.random() + np.arange(N)) / N
-
-    indexes = np.zeros(N, 'i')
-
-    i, j = 0, 0
-    while i < N:
-        if random_positions[i] < cum_sum_weights[j]:
-            indexes[i] = j
-            i += 1
-        else:
-            j += 1
-
-    return np.unique(indexes)
 
 
 
--- test_gmphd.py
+++ test_gmphd.py
@@ -20,57 +20,80 @@ class GMPHDTest(TestCase):
         self.generator.plot(show_clutter=True)
 
         self.birthgmm = []
-        for x in range(-50, 50, 5):
-            for y in range(-50, 50, 5):
+        for x in range(-50, 50, 10):
+            for y in range(-50, 50, 10):
                 target = Target(init_state=np.array([[x], [y], [0.0], [0.0]]))
                 self.birthgmm.append(target)
 
         self.filternode = PHDFilterNode(self.birthgmm)
 
-
     def tearDown(self):
         super().tearDown()
 
-    def test_target_next_state(self):
-        init_state = self.target.state
-        init_cov = self.target.state_cov
-        self.target.next_state()
-        next_state = self.target.state
-        next_cov = self.target.state_cov
+    # def test_target_next_state(self):
+    #     init_state = self.target.state
+    #     init_cov = self.target.state_cov
+    #     self.target.next_state()
+    #     next_state = self.target.state
+    #     next_cov = self.target.state_cov
+    #
+    #     assert_raises(AssertionError, assert_array_equal,
+    #                   init_state, next_state)
+    #     assert_raises(AssertionError, assert_array_equal,
+    #                   init_cov, next_cov)
+    #     assert len(self.target.all_states) == 2
+    #     assert len(self.target.all_cov) == 2
+    #
+    # def test_target_get_measurement(self):
+    #     self.target.next_state()
+    #     next_state = self.target.state
+    #     obs = self.target.get_measurement()
+    #     next_state_pos = np.array([[next_state[0][0]],
+    #                                [next_state[1][0]]])
+    #     assert_array_equal(obs, next_state_pos)
+    #
+    # def test_target_sample(self):
+    #     first_sample = self.target.sample()
+    #     self.target.next_state()
+    #     second_sample = self.target.sample()
+    #
+    #     assert first_sample.shape == second_sample.shape
+    #
+    # def test_node_predict(self):
+    #     self.filternode.predict()
+    #
+    #     assert len(self.filternode.predicted_pos) == \
+    #            len(self.filternode.predicted_targets)
+    #
+    # def test_node_update(self):
+    #     self.filternode.predict()
+    #     self.filternode.update(self.generator.observations[0])
+    #
+    #     assert len(self.filternode.updated_targets) >= \
+    #            len(self.filternode.predicted_targets)
+    #
+    # def test_node_prune(self):
+    #     self.filternode.predict()
+    #     self.filternode.update(self.generator.observations[0])
+    #     self.filternode.prune()
+    #
+    #     assert len(self.filternode.pruned_targets) <= \
+    #            len(self.filternode.updated_targets)
 
-        assert_raises(AssertionError, assert_array_equal,
-                      init_state, next_state)
-        assert_raises(AssertionError, assert_array_equal,
-                      init_cov, next_cov)
-        assert len(self.target.all_states) == 2
-        assert len(self.target.all_cov) == 2
+    # def test_node_merge(self):
+    #     self.filternode.predict()
+    #     self.filternode.update(self.generator.observations[0])
+    #     self.filternode.prune()
+    #     self.filternode.merge()
+    #     print(len(self.filternode.merged_targets))
+    #     print(len(self.filternode.pruned_targets))
+    #
+    #     assert len(self.filternode.merged_targets) <= \
+    #            len(self.filternode.pruned_targets)
 
-    def test_target_get_measurement(self):
-        self.target.next_state()
-        next_state = self.target.state
-        obs = self.target.get_measurement()
-        next_state_pos = np.array([[next_state[0][0]],
-                                   [next_state[1][0]]])
-        assert_array_equal(obs, next_state_pos)
+    def test_node_step_through(self):
+        self.filternode.step_through(self.generator.observations)
 
-    def test_target_sample(self):
-        first_sample = self.target.sample()
-        self.target.next_state()
-        second_sample = self.target.sample()
 
-        assert first_sample.shape == second_sample.shape
 
-    def test_node_predict(self):
-        old_targets = self.filternode.targets
-        self.filternode.predict()
-        new_targets = self.filternode.targets
-        print(old_targets)
-        print(new_targets)
-
-        assert len(self.filternode.predicted_pos) == len(self.filternode.predicted_targets)
-
-    def test_node_update(self):
-        self.filternode.predict()
-        self.filternode.update(self.generator.observations[0])
-        #TODO: write an actual test
 
