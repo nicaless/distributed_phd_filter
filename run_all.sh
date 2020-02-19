bd9eed2b43cc657fb5589e1f7343da83c82541d1
Nicole Fronda <nfronda@gmail.com>
2020-02-18 18:05:37 -0800

    updating bash to kick off simulations

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,14 @@
-#python -W ignore run_sim.py 3 3_nodes
-python -W ignore run_sim2.py 5 5_nodes
-python -W ignore run_sim2.py 6 6_nodes
-python -W ignore run_sim2.py 7 7_nodes
-python -W ignore run_sim2.py 10 10_nodes
-python -W ignore run_sim2.py 12 12_nodes
+python run_sim_small_test.py 3 3_nodes_small_test
+python run_sim_large_test.py 3 3_nodes_large_test
+python run_sim_large_test.py 5 5_nodes_large_test
+
+#python run_sim.py 3 3_nodes
+#python run_sim.py 5 5_nodes
+#python run_sim.py 6 6_nodes
+#python run_sim.py 7 7_nodes
+#python run_sim.py 10 10_nodes
+#python run_sim.py 12 12_nodes
+#python run_sim.py 15 15_nodes
+#python run_sim.py 20 20_nodes
+#python run_sim.py 25 25_nodes
+#python run_sim.py 30 30_nodes
