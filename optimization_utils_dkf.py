0622dd3eabcd38000a1c45686a942d3f91634190
nicaless <nfronda@gmail.com>
2020-07-07 17:44:55 -0700

    implementing branch and bound solver

--- optimization_utils_dkf.py
+++ optimization_utils_dkf.py
@@ -8,6 +8,9 @@ import picos as pic
 from picos.solvers import SolverError
 import platform
 from scipy.linalg import block_diag
+from heapq import *
+import itertools
+counter = itertools.count()
 
 
 def magnitude(x):
@@ -413,7 +416,7 @@ def team_opt_iter(adj_mat, current_weights, covariance_matrices, omegas,
 
         delta_bar = problem.add_variable('delta_bar', (p_size, p_size))
         delta_array = problem.add_variable('delta_array', (p_size, s))
-        schur = problem.add_variable('schur', (p_size * 2, p_size * 2))
+        # schur = problem.add_variable('schur', (p_size * 2, p_size * 2))
 
         # Add Params (ie constant affine expressions to help with creating constraints)
         I = pic.new_param('I', np.eye(s))
@@ -516,6 +519,296 @@ def team_opt_iter(adj_mat, current_weights, covariance_matrices, omegas,
         new_weights[i] = {}
 
     A = best_A
+    for i in range(n):
+        nw = A[i, i].value
+        if nw == 0:
+            nw = 0.1
+        new_weights[i][i] = nw
+        for j in range(i + 1, n):
+            if round(new_config[i, j]) == 1:
+                nw = A[i, j].value
+                if nw == 0:
+                    nw = 0.1
+                new_weights[i][j] = nw
+                new_weights[j][i] = nw
+    print(new_config)
+    return new_config, new_weights
+
+
+# Begin Branch and Bound Implementation of Team Optimization Problem
+
+def team_opt_bnb_greedy_heuristic(failed_node, adj_mat, covariance_matrices, tried_nodes, use_max=True):
+    det_covs = [np.linalg.det(c) for c in covariance_matrices]
+    curr_node = failed_node
+    det_covs_mask = deepcopy(det_covs)
+
+    if use_max:
+        for t in tried_nodes:
+            det_covs_mask[t] = -np.inf
+    else:
+        for t in tried_nodes:
+            det_covs_mask[t] = np.inf
+
+    curr_node_saturated = False
+    non_neigbor_idx = []
+    while len(non_neigbor_idx) == 0:
+        if curr_node_saturated:
+            if use_max:
+                det_covs_mask[curr_node] = -np.inf
+                curr_node = det_covs_mask.index(max(det_covs_mask))
+            else:
+                det_covs_mask[curr_node] = np.inf
+                curr_node = det_covs_mask.index(min(det_covs_mask))
+            curr_node_saturated = False
+        for i in range(adj_mat.shape[0]):
+            if adj_mat[i, curr_node] == 0:
+                non_neigbor_idx.append(i)
+        if len(non_neigbor_idx) == 0:
+            curr_node_saturated = True
+
+    best_cov = None
+    best_cov_id = None
+    for n in non_neigbor_idx:
+        if best_cov is None:
+            best_cov = det_covs[n]
+            best_cov_id = n
+        else:
+            if det_covs[n] < best_cov:
+                best_cov = det_covs[n]
+                best_cov_id = n
+
+    new_adj_mat = deepcopy(adj_mat)
+    new_adj_mat[curr_node, best_cov_id] = 1
+    new_adj_mat[best_cov_id, curr_node] = 1
+
+    return new_adj_mat, curr_node, best_cov_id
+
+
+def team_opt_mip(pi, adj_mat, cov_array, inv_cov_array, s):
+
+    n = adj_mat.shape[0]
+    beta = 1 / n
+    # tol = 0.00001
+    tol = 0.1
+    p_size = n * s
+
+    # Init Problem
+    problem = pic.Problem()
+
+    # Add Variables
+    A = problem.add_variable('A', adj_mat.shape,
+                             'symmetric')  # A is an SDP var
+    mu = problem.add_variable('mu', 1)  # mu is an SDP var
+
+    Pbar = problem.add_variable('Pbar', (p_size, p_size))
+
+    delta_list = []
+    for i in range(n):
+        delta_list.append(problem.add_variable('delta[{0}]'.format(i), (s, s)))
+
+    delta_bar = problem.add_variable('delta_bar', (p_size, p_size))
+    delta_array = problem.add_variable('delta_array', (p_size, s))
+    # schur = problem.add_variable('schur', (p_size * 2, p_size * 2))
+
+    # Add Params (ie constant affine expressions to help with creating constraints)
+    I = pic.new_param('I', np.eye(s))
+    Ibar = pic.new_param('Ibar', np.eye(p_size))
+    cov_array_param = pic.new_param('covs', cov_array)
+    inv_cov_array_param = pic.new_param('inv_covs', inv_cov_array)
+    PI = pic.new_param('PI', pi)
+
+    # Set Objective
+    problem.set_objective('min', beta * pic.trace(Pbar))
+
+    # Constraints
+
+    # Setting Additional Constraint such that delta_bar elements equal elements in delta_list (with some tolerance)
+    for i in range(n):
+        start = i * s
+        end = i * s + s
+        problem.add_constraint(abs(delta_bar[start:end, start:end] - delta_list[i]) <= tol)
+        if i < (n - 1):
+            # Fill everything to left with 0s
+            problem.add_constraint(delta_bar[start:end, end:] == np.zeros((s, (n * s) - end)))
+            # Fill everything below with 0s
+            problem.add_constraint(delta_bar[end:, start:end] == np.zeros(((n * s) - end, s)))
+
+    # Setting Additional Constraint such that delta_array elements equal elements in delta_list (with some tolerance)
+    for i in range(n):
+        start = i * s
+        end = i * s + s
+        problem.add_constraint(abs(delta_array[start:end, :] - delta_list[i]) <= tol)
+
+    # Setting Additional Constraint such that delta_bar and Pbar elements in schur variable (with some tolerance)
+    # problem.add_constraint(abs(schur[0:p_size, 0:p_size] - Pbar) <= tol)
+    # problem.add_constraint(abs(schur[p_size:, p_size:] - delta_bar) <= tol)
+    # problem.add_constraint(schur[0:p_size, p_size:] == np.eye(p_size))
+    # problem.add_constraint(schur[p_size:, 0:p_size] == np.eye(p_size))
+
+    # Schur constraint
+    # problem.add_constraint(schur >= 0)
+    problem.add_constraint(((Pbar & Ibar) //
+                           (Ibar & delta_bar)).hermitianized >> 0)
+
+    # Kron constraint
+    problem.add_constraint(pic.kron(A, I) * cov_array_param +
+                           inv_cov_array_param == delta_array)
+    # problem.add_constraint(pic.kron(A, I) * cov_array_param == delta_array)
+
+    # Set the usual Constraints
+    problem.add_constraint(mu >= 0.001)
+    problem.add_constraint(mu < 1)
+    problem.add_constraint(
+        (A * np.ones((n, 1))) == np.ones((n, 1)))  # Constraint 1
+    problem.add_constraint((beta * np.dot(np.ones(n).T, np.ones(n))) +
+                           (1 - mu) * np.eye(n) >= A)  # Constraint 2
+
+    for i in range(n):
+        problem.add_constraint(A[i, i] > 0)  # Constraint 6
+        for j in range(n):
+            if i == j:
+                continue
+                # problem.add_constraint(PI[i, j] == 1.0)  # Constraint 3
+            else:
+                problem.add_constraint(A[i, j] > 0)  # Constraint 7
+                problem.add_constraint(A[i, j] <= PI[i, j])  # Constraint 8
+
+    # problem.add_constraint(
+    #     abs(PI - adj_mat) ** 2 <= edge_mod_limit)  # Constraint 9
+
+    sol = problem.solve(verbose=0, solver='mosek')
+    obj = sol.value
+
+    return obj, problem, A
+
+
+class BBTreeNode():
+    def __init__(self, pi, tried_nodes, adj_mat, current_weights, covariance_matrices, omegas, failed_node, ne=1):
+        n = adj_mat.size[0]
+        s = covariance_matrices[0].shape[0]
+
+        # Reducing Magnitude if necessary
+        mag_max_val_in_matrices = magnitude(max([np.max(abs(cov))
+                                                 for cov in covariance_matrices]))
+        if mag_max_val_in_matrices > 15:
+            print('rescaling matrix magnitude, magnitude too high')
+            covariance_matrices = [cov * (10 ** (-1 * mag_max_val_in_matrices))
+                                   for cov in covariance_matrices]
+
+        cov_array = np.zeros((n * s, s))
+        for i in range(n):
+            start = i * s
+            end = i * s + s
+            cov_array[start:end, 0:s] = omegas[i]
+
+        inv_cov_array = np.zeros((n * s, s))
+        for i in range(n):
+            start = i * s
+            end = i * s + s
+            inv_cov_array[start:end, 0:s] = np.linalg.inv(covariance_matrices[i])
+
+        self.pi = pi
+        self.tried_nodes = tried_nodes
+
+        self.failed_node = failed_node
+
+        self.adj_mat = adj_mat
+        self.current_weights = current_weights
+        self.covariance_matrices = covariance_matrices
+        self.omegas = omegas
+        self.ne = ne
+
+        self.s = s
+        self.cov_array = cov_array
+        self.inv_cov_array = inv_cov_array
+
+        self.children = []
+        self.solved_problem = None
+
+    def buildSolveProblem(self):
+        pi = self.pi
+        adj_mat = self.adj_mat
+        cov_array = self.cov_array
+        inv_cov_array = self.inv_cov_array
+        s = self.s
+        obj, problem, A = team_opt_mip(pi, adj_mat, cov_array, inv_cov_array, s)
+        self.solved_problem = A
+        return obj, problem, A
+
+    def branch(self):
+        children = []
+        for b in [0, 1]:
+            pi, curr_node, best_cov_id = team_opt_bnb_greedy_heuristic(self.failed_node,
+                                                                       self.adj_mat,
+                                                                       self.covariance_matrices,
+                                                                       self.tried_nodes,
+                                                                       use_max=b==0)
+            self.tried_nodes.append(best_cov_id)
+            n1 = BBTreeNode(pi,
+                            self.tried_nodes,  # inherit parent's tried nodes
+                            self.adj_mat,
+                            self.current_weights,
+                            self.covariance_matrices,
+                            self.omegas,
+                            curr_node,
+                            ne=self.ne)
+            children.append(n1)
+        return children
+
+    def bbsolve(self):
+        root = self
+
+        bestres = 1e20  # a big arbitrary initial best objective value
+        bestnode = root  # initialize bestnode to the root
+
+        res = root.buildSolveProblem()
+        heap = [(res, next(counter), root)]
+
+        A = None
+        nodecount = 0
+        while len(heap) > 0:
+            nodecount += 1  # for statistics
+            print("Heap Size: ", len(heap))
+            _, _, node = heappop(heap)
+            obj, problem, A = node.buildSolveProblem()
+            print("Result: ", obj)
+            if problem.status in ['integer optimal', 'optimal']:
+                if obj > bestres - 1e-3:  # even the relaxed problem sucks. forget about this branch then
+                    print("Relaxed Problem Stinks. Killing this branch.")
+                    pass
+                elif problem.status in ['integer optimal']:  #if a valid solution then this is the new best
+                    print("New Best Integral solution.")
+                    bestres = obj
+                    bestnode = node
+
+                # otherwise, we're unsure if this branch holds promise.
+                # Maybe it can't actually achieve this lower bound. So branch into it
+                else:
+                    new_nodes = node.branch()
+                    for new_node in new_nodes:
+                        # using counter to avoid possible comparisons between nodes. It tie breaks
+                        heappush(heap, (res, next(counter), new_node))
+        print("Nodes searched: ", nodecount)
+        return bestres, bestnode, A
+
+
+def team_opt_bnb(adj_mat, current_weights, covariance_matrices, omegas, failed_node, ne=1):
+    pi, curr_node, best_cov_id = team_opt_bnb_greedy_heuristic(failed_node, adj_mat, covariance_matrices, [])
+    tried_nodes = [best_cov_id]
+    root = BBTreeNode(pi, tried_nodes, adj_mat, current_weights, covariance_matrices, omegas, failed_node, ne=ne)
+    bestres, bestnode, A = root.bbsolve()
+    print("best solution value: ", bestres)
+
+    if A is None:
+        return adj_mat, current_weights
+
+    new_config = bestnode.pi
+
+    n = adj_mat.shape[0]
+    new_weights = {}
+    for i in range(n):
+        new_weights[i] = {}
+
     for i in range(n):
         nw = A[i, i].value
         if nw == 0:
